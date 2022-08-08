f2b45f6b4d7fbea4bb27e503ab92e1e9dc0cceb4
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-08-08 17:23:26 -0400

    setting up errors , router and controller

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const urlsRouter = require("./urls/urls.router");
+const usesrouter = require("./uses/uses.router");
 
 const app = express();
 
 app.use(express.json());
+app.use("/urls", urlsRouter);
+app.use("/uses", usesrouter);
 
 // TODO: Add code to meet the requirements and make the tests pass.
+
+//Not found errors
+app.use((req, res, next) => {
+  next(`Not found : ${req.originalUrls}`);
+});
+
+//error handler
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;

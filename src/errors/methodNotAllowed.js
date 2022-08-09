a0696dd3dd4b0b2212a2d20f9fbcdad41b7b76e8
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-08-09 12:45:45 -0400

    methodenotAllowed

--- src/errors/methodNotAllowed.js
+++ src/errors/methodNotAllowed.js
@@ -1,7 +1,7 @@
 const methodNotAllowed = (req, res, next) => {
   next({
     status: 405,
-    message: `${req.method} not allowed for ${req.originalUrl}`,
+    message: `${req.method} not allowed on ${req.originalUrl}`,
   });
 };
 
